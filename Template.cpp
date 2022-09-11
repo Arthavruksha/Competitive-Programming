45940dcf781ff1247332e1ac6f00c86354e99a73
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-09-11 11:25:32 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+} 
+
+
+int main() {
+	OnlineJudge();
+
+    return 0;
+}
+
