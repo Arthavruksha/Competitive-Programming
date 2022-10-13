0931210896eb96658f61bf5c5fef148505ec5624
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-13 19:38:06 +0530

    Create 2^N.cpp

---
+++ Atcoder/2^N.cpp
@@ -0,0 +1,26 @@
+// Problem Link - https://atcoder.jp/contests/abc256/tasks/abc256_a
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	int n;
+	cin>>n;
+	cout<<(1 << n)<<endl;
+	
+
+	return 0;
+}
