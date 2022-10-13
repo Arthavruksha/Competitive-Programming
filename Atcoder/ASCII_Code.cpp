b78c80107239d88a8dceef58cdcf2e80a3c280b2
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-13 23:34:28 +0530

    Create ASCII_Code.cpp

---
+++ Atcoder/ASCII_Code.cpp
@@ -0,0 +1,28 @@
+// Problem Link - https://atcoder.jp/contests/abc252/tasks/abc252_a
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	int n;
+	cin>>n;
+
+	int ans = n-97;
+
+	cout<<char('a' + ans )<<endl;
+
+	return 0;
+}
