a497312b2ec56216cfb487d1a6c570afc21a0672
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-13 19:49:45 +0530

    Create Last_Two_Digits.cpp

---
+++ Atcoder/Last_Two_Digits.cpp
@@ -0,0 +1,27 @@
+// Problem Link - https://atcoder.jp/contests/abc254/tasks/abc254_a
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	string n;
+	cin>>n;
+	cout<<n.substr(1,2)<<endl;
+
+	
+
+	return 0;
+}
