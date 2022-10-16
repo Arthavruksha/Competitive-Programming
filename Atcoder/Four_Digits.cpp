9f545da1de3aca6e0ff36c7832bbc34daa8d442f
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-16 19:07:19 +0530

    Create Four_Digits.cpp

---
+++ Atcoder/Four_Digits.cpp
@@ -0,0 +1,25 @@
+// Problem Link - https://atcoder.jp/contests/abc222/tasks/abc222_a
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+	
+	int n;
+	cin>>n;
+	cout<< setw(4) << setfill('0') << n << endl;
+
+	return 0;
+}
