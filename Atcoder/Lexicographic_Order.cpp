923e54666497bad684dccc621f575f87c384d674
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-16 23:44:14 +0530

    Create Lexicographic_Order.cpp

---
+++ Atcoder/Lexicographic_Order.cpp
@@ -0,0 +1,28 @@
+// Problem Link - https://atcoder.jp/contests/abc217/tasks/abc217_a
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	string s,t;
+	cin>>s>>t;
+
+	if (s<t)cout<<"Yes"<<endl;
+	else cout<<"No"<<endl;
+
+	return 0;
+}
