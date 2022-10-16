0c348537424baa84e8e3a6ea598af14fa3f04af4
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-16 13:25:17 +0530

    Create Round_Decimals.cpp

---
+++ Atcoder/Round_Decimals.cpp
@@ -0,0 +1,28 @@
+// Problem Link - https://atcoder.jp/contests/abc226/tasks/abc226_a
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	float x;
+	cin>>x;
+
+	cout << round(x) << endl;
+
+
+	return 0;
+}
