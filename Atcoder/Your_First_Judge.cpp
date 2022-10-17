9f3499e5a6de938c3c0da346bc700b48075c52be
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-17 16:27:39 +0530

    Create Your_First_Judge.cpp

---
+++ Atcoder/Your_First_Judge.cpp
@@ -0,0 +1,27 @@
+// Problem Link - https://atcoder.jp/contests/abc215/tasks/abc215_a
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	string s;
+	cin>>s;
+
+	if (s == "Hello,World!") cout<<"AC"<<endl;
+	else cout<<"WA"<<endl;
+
+
+	return 0;
+}
