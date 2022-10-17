14ac31204f0d2edaf2a22fcded5450c25549402a
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-17 18:29:14 +0530

    Create Rotate_.cpp

---
+++ Atcoder/Rotate_.cpp
@@ -0,0 +1,26 @@
+// Problem Link - https://atcoder.jp/contests/abc197/tasks/abc197_a
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	string s;
+	cin>>s;
+
+	cout << s[1] << s[2] << s[0] << endl;
+
+	return 0;
+}
