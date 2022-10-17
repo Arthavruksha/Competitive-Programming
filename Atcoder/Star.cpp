02daff45bce80276bf46e2a68c698919aa1eab27
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-17 18:54:12 +0530

    Create Star.cpp

---
+++ Atcoder/Star.cpp
@@ -0,0 +1,27 @@
+// Problem Link - https://atcoder.jp/contests/abc192/tasks/abc192_a
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	int n;
+	cin>>n;
+
+	cout<<(100 - (n%100))<<endl;
+
+
+	return 0;
+}
