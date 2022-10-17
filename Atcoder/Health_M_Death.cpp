334a81585e6ca1c4a6633c6ea8f6e808e2c8e630
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-17 18:47:42 +0530

    Create Health_M_Death.cpp

---
+++ Atcoder/Health_M_Death.cpp
@@ -0,0 +1,29 @@
+// Problem Link - https://atcoder.jp/contests/abc195/tasks/abc195_a
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	int m,h;
+	cin>>m>>h;
+
+	if ( h%m == 0 ) cout<<"Yes"<<endl;
+	else cout<<"No"<<endl;
+
+
+
+	return 0;
+}
