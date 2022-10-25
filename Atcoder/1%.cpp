6440808485d966641c3d98f2d808af5ce140538e
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-25 16:42:49 +0530

    Create 1%.cpp

---
+++ Atcoder/1%.cpp
@@ -0,0 +1,36 @@
+// Problem Link - https://atcoder.jp/contests/abc168/tasks/abc168_b
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	ll x;
+	cin>>x;
+
+	ll temp = 100;
+	ll ways = 0;
+	while(temp<x){
+		temp += temp/100;
+		ways++;
+	}
+
+	cout<<ways<<endl;
+
+
+	return 0;
+
+}
+
