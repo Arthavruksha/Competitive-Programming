5f5f9a220f1a628f530a69a2ffceb3126e7c531c
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-10-26 15:20:15 +0530

    Create I_Miss_You.cpp

---
+++ Atcoder/I_Miss_You.cpp
@@ -0,0 +1,35 @@
+// Problem Link - https://atcoder.jp/contests/abc154/tasks/abc154_b
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	string s;
+	cin>>s;
+
+	ll n = s.size();
+
+	for(int i=0;i<n;i++){
+		cout<<"x";
+	}
+
+	cout<<endl;
+
+
+	return 0;
+
+}
+
