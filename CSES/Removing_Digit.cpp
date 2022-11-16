0fd025b0ad59d1b6b0122ce162ad16030f562df5
Jay Shankarpure <76587144+Arthavruksha@users.noreply.github.com>
2022-11-16 22:45:45 +0530

    Update Removing_Digit.cpp

--- CSES/Removing_Digit.cpp
+++ CSES/Removing_Digit.cpp
@@ -1,27 +1,45 @@
-// Problem Link - https://cses.fi/problemset/task/1637/
+// Problem Link - https://cses.fi/problemset/task/1637
 
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const int mod = 1e9+7;
-#define INF 1e9
- 
- 
-int main() {
-    int n;
-    cin>>n;
-    vector <ll> dp (n+1,1e9);
-    
-    
-    dp[0] = 0;
-    
-    for (int i =0;i<=n;i++){
-        for (char c : to_string(i) ){
-            dp[i] = min( dp[i] , dp[ i - (c-'0') ]+1 );
-        }
-    }
-    
-    cout<<dp[n]<<endl;
- 
-    return 0;
+#define ll long long 
+
+void OnlineJudge () {
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
 }
+
+int main() {
+	OnlineJudge();
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0);
+
+	// state = dp[x] = min no.of ways to make x 0 by subtracting max digit from the x number 
+	// transition = dp[i] += dp[i-ans]
+    // base case = dp[0] = 1;
+    // final sub-problem  = dp[n]
+
+    ll n;
+    cin>>n;
+
+    vector <ll> dp(n+1);
+    dp[0] = 0;
+
+    for(int i=1;i<=n;i++){
+    	string s = to_string(i);
+    	int ans = 0;
+    	for ( char j : s ) { 
+    		int temp = int(j-48) ;
+    		ans = max(ans,temp);
+    	}
+    	dp[i] += dp[i-ans] + 1 ;
+    }
+
+    cout<<dp[n]<<endl;
+
+
+	return 0;
+}
+
